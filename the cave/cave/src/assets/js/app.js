0e75086e3920f2984746d49fb9aa23da23bc662a
Rob Aperios <roberteaperios@gmail.com>
2017-04-27 13:05:13 +0200

    updated grid with responsive content

--- the cave/cave/src/assets/js/app.js
+++ the cave/cave/src/assets/js/app.js
@@ -1,1 +1,17 @@
-$(document).foundation();
+$(document).ready( function() {
+  	$(document).foundation();
+	var nrOfBlocks = 100;
+
+	var gridSquareSize = $(document).width() / 7;
+
+
+  var output = "";
+
+  for (var i = 0; i < nrOfBlocks; i++) {
+    output += "<div class=\"grid-block\" > " + i + "</div> ";
+
+  }
+
+  
+  $("#cave-grid").html(output);
+});
