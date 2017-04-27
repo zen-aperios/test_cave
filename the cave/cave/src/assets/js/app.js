3029ac230a9caeafca883b2d1fc644229f69ced8
Rob Aperios <roberteaperios@gmail.com>
2017-04-27 13:26:20 +0200

    grid changes

--- the cave/cave/src/assets/js/app.js
+++ the cave/cave/src/assets/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready( function() {
   	$(document).foundation();
-	var nrOfBlocks = 100;
+/*	var nrOfBlocks = 100;
 
 	var gridSquareSize = $(document).width() / 7;
 
@@ -8,10 +8,10 @@ $(document).ready( function() {
   var output = "";
 
   for (var i = 0; i < nrOfBlocks; i++) {
-    output += "<div class=\"grid-block\" > " + i + "</div> ";
+    output += "<div class=\"grid-block\" > <div class=\"grid-bloc-header\">asd</div> </div> ";
 
   }
 
   
-  $("#cave-grid").html(output);
+  $("#cave-grid").html(output); */
 });
