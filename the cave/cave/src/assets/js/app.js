d0e84dab68604ee788071cc13e9a2ce3d603d53a
Rob Aperios <roberteaperios@gmail.com>
2017-04-28 14:53:04 +0200

    grid tests

--- the cave/cave/src/assets/js/app.js
+++ the cave/cave/src/assets/js/app.js
@@ -1,5 +1,13 @@
 $(document).ready( function() {
   	$(document).foundation();
+
+  	$('.off-canvas-wrapper').foundation('offcanvas', 'show', 'move-right');
+	$('.off-canvas-wrapper').foundation('offcanvas', 'toggle', 'move-right');
+  	$('.off-canvas').foundation('offcanvas', 'show', 'move-right');
+	$('.off-canvas').foundation('offcanvas', 'toggle', 'move-right');	
+	//$('.off-canvas-wrapper').foundation(data-content-overlay);
+	//foudata-content-overlay
+
 /*	var nrOfBlocks = 100;
 
 	var gridSquareSize = $(document).width() / 7;
