220c41b0ea646e35a8f84675a10ae6c8e8d83fd5
Rob Aperios <roberteaperios@gmail.com>
2017-05-11 10:18:37 +0200

    Function replacement

--- the cave/cave/src/assets/js/app.js
+++ the cave/cave/src/assets/js/app.js
@@ -1,2 +1,18 @@
 $(document).foundation();
+$(document).ready( function() {
+  	$(document).foundation();
+/*	var nrOfBlocks = 100;
 
+	var gridSquareSize = $(document).width() / 7;
+
+
+  var output = "";
+
+  for (var i = 0; i < nrOfBlocks; i++) {
+    output += "<div class=\"grid-block\" > <div class=\"grid-bloc-header\">asd</div> </div> ";
+
+  }
+
+  
+  $("#cave-grid").html(output); */
+})
