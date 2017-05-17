52b9dd5afcd90dd2903f691d41c14eb9f2853395
Rob Aperios <roberteaperios@gmail.com>
2017-05-17 12:37:56 +0200

    images-  critical changes

--- the cave/cave/src/assets/js/app.js
+++ the cave/cave/src/assets/js/app.js
@@ -1,18 +1,92 @@
 $(document).foundation();
-$(document).ready( function() {
-  	$(document).foundation();
-/*	var nrOfBlocks = 100;
 
-	var gridSquareSize = $(document).width() / 7;
+	/* $(document).ready( function() {
+	  	$(document).foundation();
+		var nrOfBlocks = 100;
+
+		var gridSquareSize = $(document).width() / 7;
 
 
-  var output = "";
+	  var output = "";
 
-  for (var i = 0; i < nrOfBlocks; i++) {
-    output += "<div class=\"grid-block\" > <div class=\"grid-bloc-header\">asd</div> </div> ";
+	  for (var i = 0; i < nrOfBlocks; i++) {
+	    output += "<div class=\"grid-block\" > <div class=\"grid-bloc-header\">asd</div> </div> ";
 
-  }
+	  }
 
-  
-  $("#cave-grid").html(output); */
-})
+	  
+	  $("#cave-grid").html(output); 
+	}); */
+
+
+
+
+// --------- Part - 3 ------------
+
+$('#project-filter').click(function (a) {  
+
+
+    $(".projects-cave").toggleClass("projects-expand");
+    $("#project-output").toggleClass("output-expand");
+
+   	$(".style-cave").removeClass("style-expand");
+    $("#style-output").removeClass("output-expand");    
+
+    $(".sequence-cave").removeClass("sequence-expand");
+    $("#sequence-output").removeClass("output-expand");
+
+    $(".size-cave").removeClass("size-expand");
+    $("#size-output").removeClass("output-expand");
+
+ });
+ $('#style-filter').click(function (b) {  
+
+
+    $(".style-cave").toggleClass("style-expand");
+    $("#style-output").toggleClass("output-expand");
+
+    $(".projects-cave").removeClass("projects-expand");
+    $("#project-output").removeClass("output-expand");
+
+    $(".sequence-cave").removeClass("sequence-expand");
+    $("#sequence-output").removeClass("output-expand");
+
+    $(".size-cave").removeClass("size-expand");
+    $("#size-output").removeClass("output-expand");
+
+
+ });   
+ $('#sequence-filter').click(function (c) {  
+
+
+    $(".sequence-cave").toggleClass("sequence-expand");
+    $("#sequence-output").toggleClass("output-expand");
+
+    $(".style-cave").removeClass("style-expand");
+    $("#style-output").removeClass("output-expand");  
+
+    $(".projects-cave").removeClass("projects-expand");
+    $("#project-output").removeClass("output-expand");    
+
+    $(".size-cave").removeClass("size-expand");
+    $("#size-output").removeClass("output-expand");
+
+
+ });   
+ $('#size-filter').click(function (d) {  
+
+
+    $(".size-cave").toggleClass("size-expand");
+    $("#size-output").toggleClass("output-expand");
+
+    $(".sequence-cave").removeClass("sequence-expand");
+    $("#sequence-output").removeClass("output-expand");    
+
+    $(".style-cave").removeClass("style-expand");
+    $("#style-output").removeClass("output-expand");   
+    
+   $(".projects-cave").removeClass("projects-expand");
+    $("#project-output").removeClass("output-expand");    
+       
+
+ });      
